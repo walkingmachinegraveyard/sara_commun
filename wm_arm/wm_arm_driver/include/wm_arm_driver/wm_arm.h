e64576955eb764c23933512427a0aef61dc868d4
xhache <xvr.hache@gmail.com>
2016-05-29 21:50:58 -0400

    planning overhaul

--- wm_arm/wm_arm_driver/include/wm_arm_driver/wm_arm.h
+++ wm_arm/wm_arm_driver/include/wm_arm_driver/wm_arm.h
@@ -28,21 +28,24 @@ namespace manipulator
 	class wmArm
 	{
 		public:
-			wmArm(ros::NodeHandle&, kinova::kinovaComm&, const moveit::planning_interface::MoveGroup::Options&);
+			wmArm(ros::NodeHandle&, kinova::kinovaComm&, const moveit::planning_interface::MoveGroup::Options&, const moveit::planning_interface::MoveGroup::Options&);
 			~wmArm();
 			bool stopControlService(wm_arm_msgs::stopArmControl::Request& req, wm_arm_msgs::stopArmControl::Response& res);
 			bool startControlService(wm_arm_msgs::startArmControl::Request& req, wm_arm_msgs::startArmControl::Response& res);
 			bool homeArmService(wm_arm_msgs::homeArm::Request& req, wm_arm_msgs::homeArm::Response& res);
 			bool recoverTorqueFaultService(wm_arm_msgs::recoverTorqFault::Request& req, wm_arm_msgs::recoverTorqFault::Response& res);
 			bool armStatusService(wm_arm_msgs::armStatus::Request& req, wm_arm_msgs::armStatus::Response& res);
-			bool computePlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res);
+			bool computeBasePlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res);
+			bool computeManipulatorPlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res);
 
 		private:
 			void torqueCallback(const ros::TimerEvent&);
 			void positionCallback(const ros::TimerEvent&);
-			void velocityCallback(const ros::TimerEvent&);
+//			void velocityCallback(const ros::TimerEvent&);
 			int recoverTorqueFault();
-			bool computePlan(const geometry_msgs::Pose&, moveit::planning_interface::MoveGroup::Plan&);
+			bool computeBasePlan(const geometry_msgs::PoseStamped&, moveit::planning_interface::MoveGroup::Plan&);
+			bool computeManipulatorPlan(const geometry_msgs::PoseStamped&, moveit::planning_interface::MoveGroup::Plan&);
+			bool computeManipulatorPlan(const std::vector<double>&, moveit::planning_interface::MoveGroup::Plan&);
 
 			ros::NodeHandle nh_;
 			kinova::kinovaComm kComm_;
@@ -54,22 +57,24 @@ namespace manipulator
 			ros::ServiceServer homeArmSrv_;
 			ros::ServiceServer recoverTorqueFaultSrv_;
 			ros::ServiceServer statusSrv_;
-			ros::ServiceServer computePlanSrv_;
+			ros::ServiceServer computeBasePlanSrv_, computeManipulatorPlanSrv_;
 			ros::Publisher jointStatePub_;
 
 			AngularPosition torqueInfo_;
 			AngularPosition angularInfo_;
-			AngularPosition velocityInfo_;
-			std::vector<double> homePosition_;
+//			AngularPosition velocityInfo_;
 
-			unsigned int nbJoints_;
+			unsigned int baseGroupNbJoints_, manipulatorGroupNbJoints_;
 			std::string frameId_;
-			std::vector<std::string> activeJoints_;
+			std::vector<std::string> baseGroupActiveJoints_, manipulatorGroupActiveJoints_;
+			std::string baseEndEffectorName_, manipulatorEndEffectorName_;
 
 			bool torqueFault_;
 			float maxTorqJoint1_, maxTorqJoint2_, maxTorqJoint3_, maxTorqJoint4_, maxTorqJoint5_, maxTorqJoint6_;
+			std::vector<float> jointsOffset_;
 
-			moveit::planning_interface::MoveGroup moveGroup_;
+			moveit::planning_interface::MoveGroup baseMoveGroup_;
+			moveit::planning_interface::MoveGroup manipulatorMoveGroup_;
 	};
 } //namespace manipulator
 
--- wm_arm/wm_arm_driver/src/wm_arm.cpp
+++ wm_arm/wm_arm_driver/src/wm_arm.cpp
@@ -10,43 +10,71 @@
 
 namespace manipulator
 {
-	wmArm::wmArm(ros::NodeHandle& nh, kinova::kinovaComm& kComm, const moveit::planning_interface::MoveGroup::Options& opt)
-				:nh_(nh), kComm_(kComm), moveGroup_(opt)
+
+	inline double degToRad(float angle)
+	{
+		return (double)(M_PI * angle / 180.0);
+	}
+
+	wmArm::wmArm(ros::NodeHandle& nh, kinova::kinovaComm& kComm, const moveit::planning_interface::MoveGroup::Options& baseOpt, const moveit::planning_interface::MoveGroup::Options& manipulatorOpt)
+				:nh_(nh), kComm_(kComm), baseMoveGroup_(baseOpt), manipulatorMoveGroup_(manipulatorOpt)
 	{
 		torqueFault_ = false;
 
 		// Init structures
 		torqueInfo_.InitStruct();
 		angularInfo_.InitStruct();
-		velocityInfo_.InitStruct();
-
-		double tmpTorque;
-		nh_.param("maxTorque_joint1", tmpTorque, 8.0);
-		maxTorqJoint1_ = (float)tmpTorque;
-		nh_.param("maxTorque_joint2", tmpTorque, 8.0);
-		maxTorqJoint2_ = (float)tmpTorque;
-		nh_.param("maxTorque_joint3", tmpTorque, 7.0);
-		maxTorqJoint3_ = (float)tmpTorque;
-		nh_.param("maxTorque_joint4", tmpTorque, 6.0);
-		maxTorqJoint4_ = (float)tmpTorque;
-		nh_.param("maxTorque_joint5", tmpTorque, 4.0);
-		maxTorqJoint5_ = (float)tmpTorque;
-		nh_.param("maxTorque_joint1", tmpTorque, 4.0);
-		maxTorqJoint6_ = (float)tmpTorque;
+//		velocityInfo_.InitStruct();	Not used for now
 
 		// get robot information
-		nbJoints_ = moveGroup_.getVariableCount();
-		ROS_INFO("Move group has %d joints", nbJoints_);
-
-		frameId_ = moveGroup_.getPlanningFrame();
+		frameId_ = baseMoveGroup_.getPlanningFrame();
 		ROS_INFO("Planning frame is %s", frameId_.c_str());
 
-		activeJoints_ = moveGroup_.getActiveJoints();
+		baseGroupActiveJoints_ = baseMoveGroup_.getActiveJoints();
+		baseGroupNbJoints_ = baseGroupActiveJoints_.size();
 
-		for (unsigned int j = 0; j < activeJoints_.size(); j++)
+		manipulatorGroupActiveJoints_ = manipulatorMoveGroup_.getActiveJoints();
+		manipulatorGroupNbJoints_ = manipulatorGroupActiveJoints_.size();
+
+		for (unsigned int j = 0; j < baseGroupNbJoints_; j++)
 		{
-			ROS_INFO("Joint %d name is %s", j, activeJoints_.at(j).c_str());
+			ROS_INFO("Joint %d name is %s", j, baseGroupActiveJoints_.at(j).c_str());
 		}
+
+		baseEndEffectorName_ = baseMoveGroup_.getEndEffector();
+		manipulatorEndEffectorName_ = manipulatorMoveGroup_.getEndEffector();
+
+		// get parameters from the parameter server
+		double tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint1", tmpTorque, 8.0);
+		maxTorqJoint1_ = (float)tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint2", tmpTorque, 8.0);
+		maxTorqJoint2_ = (float)tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint3", tmpTorque, 7.0);
+		maxTorqJoint3_ = (float)tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint4", tmpTorque, 6.0);
+		maxTorqJoint4_ = (float)tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint5", tmpTorque, 4.0);
+		maxTorqJoint5_ = (float)tmpTorque;
+		nh_.param("/wm_arm_driver_node/max_torque_joint1", tmpTorque, 4.0);
+		maxTorqJoint6_ = (float)tmpTorque;
+
+		jointsOffset_ = kComm_.getJointsOffset();
+
+		// moveIt! related parameters
+		double planningTime;
+		nh_.param("/wm_arm_driver_node/planning_time", planningTime, 10.0);
+		baseMoveGroup_.setPlanningTime(planningTime);
+
+		int planningAttempts;
+		nh_.param("/wm_arm_driver_node/planning_attempts", planningAttempts, 1);
+		baseMoveGroup_.setNumPlanningAttempts(planningAttempts);
+
+		double positionTol, orientationTol;
+		nh_.param("/wm_arm_driver_node/goal_position_tolerance", positionTol, 0.01);
+		baseMoveGroup_.setGoalPositionTolerance(positionTol);
+		nh_.param("/wm_arm_driver_node/goal_orientation_tolerance", orientationTol, 0.1);
+		baseMoveGroup_.setGoalOrientationTolerance(orientationTol);
 
 		// set services
 		stopControlSrv_ = nh_.advertiseService("stop_control", &wmArm::stopControlService, this);
@@ -54,22 +82,24 @@ namespace manipulator
 		homeArmSrv_ = nh_.advertiseService("home", &wmArm::homeArmService, this);
 		recoverTorqueFaultSrv_ = nh_.advertiseService("recover_torque_fault", &wmArm::recoverTorqueFaultService, this);
 		statusSrv_ = nh_.advertiseService("status", &wmArm::armStatusService, this);
-		computePlanSrv_ = nh_.advertiseService("compute_plan", &wmArm::computePlanService, this);
+		computeBasePlanSrv_ = nh_.advertiseService("compute_base_plan", &wmArm::computeBasePlanService, this);
+		computeManipulatorPlanSrv_ = nh_.advertiseService("compute_manipulator_plan", &wmArm::computeManipulatorPlanService, this);
 
 		// set publishers
+		jointStatePub_ = nh_.advertise<sensor_msgs::JointState>("/joint_states", 1);
+
 		double torqueRate;
-		nh_.param("checkTorque_rate", torqueRate, 20.0);
+		nh_.param("/wm_arm_driver_node/torque_callback_rate", torqueRate, 20.0);
 		TorqueCbTimer_ = nh_.createTimer(ros::Duration(1.0/torqueRate), &wmArm::torqueCallback, this);
 
 		double positionRate;
-		nh_.param("checkPosition_rate", positionRate, 20.0);
+		nh_.param("/wm_arm_driver_node/position_callback_rate", positionRate, 20.0);
 		PositionCbTimer_ = nh_.createTimer(ros::Duration(1.0/positionRate), &wmArm::positionCallback, this);
-
+/*
 		double velocityRate;
-		nh_.param("checkVelocity_rate", velocityRate, 20.0);
+		nh_.param("/wm_arm_driver_node/velocity_callback_rate", velocityRate, 20.0);
 		VelocityCbTimer_ = nh_.createTimer(ros::Duration(1.0/velocityRate), &wmArm::velocityCallback, this);
-
-		jointStatePub_ = nh_.advertise<sensor_msgs::JointState>("joint_states", 1);
+		*/
 	}
 
 	wmArm::~wmArm()
@@ -80,6 +110,8 @@ namespace manipulator
 	bool wmArm::stopControlService(wm_arm_msgs::stopArmControl::Request& req, wm_arm_msgs::stopArmControl::Response& res)
 	{
 		int status = kComm_.myStopControlAPI();
+
+		kComm_.myEraseAllTrajectories();
 
 		if (status == NO_ERROR_KINOVA)
 		{
@@ -109,7 +141,7 @@ namespace manipulator
 
 	bool wmArm::homeArmService(wm_arm_msgs::homeArm::Request& req, wm_arm_msgs::homeArm::Response& res)
 	{
-		int status = kComm_.myMoveHome(homePosition_);
+		int status = kComm_.myMoveHome();
 
 		if (status == NO_ERROR_KINOVA)
 		{
@@ -139,15 +171,59 @@ namespace manipulator
 		return true;
 	}
 
-	bool wmArm::computePlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res)
+	bool wmArm::computeBasePlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res)
+	{
+		if (req.planningSpace != req.CARTESIAN_SPACE)
+		{
+			res.planningResult = res.PLANNING_FAILURE;
+			ROS_ERROR("Planning with base requires a cartesian pose.");
+		}
+
+		else
+		{
+			moveit::planning_interface::MoveGroup::Plan myPlan;
+
+			bool success = computeBasePlan(req.targetPose, myPlan);
+
+			if (success)
+			{
+				res.planningResult = res.PLANNING_SUCCESS;
+				res.trajectory = myPlan.trajectory_;
+			}
+			else
+			{
+				res.planningResult = res.PLANNING_FAILURE;
+			}
+		}
+
+		return true;
+	}
+
+	bool wmArm::computeManipulatorPlanService(wm_arm_msgs::computePlan::Request& req, wm_arm_msgs::computePlan::Response& res)
 	{
 		moveit::planning_interface::MoveGroup::Plan myPlan;
-		bool success = computePlan(req.targetPose, myPlan);
+		bool success = false;
+
+		if (req.planningSpace == req.CARTESIAN_SPACE)
+		{
+			success = computeManipulatorPlan(req.targetPose, myPlan);
+		}
+		else if (req.planningSpace == req.JOINT_SPACE)
+		{
+			if (req.jointPos.size() != manipulatorGroupNbJoints_)
+			{
+				ROS_ERROR("Planning: number of joints mismatch");
+			}
+			success = computeManipulatorPlan(req.jointPos, myPlan);
+		}
+		else
+		{
+			ROS_ERROR("Unknown planning space.");
+		}
 
 		if (success)
 		{
 			res.planningResult = res.PLANNING_SUCCESS;
-			res.startState = myPlan.start_state_;
 			res.trajectory = myPlan.trajectory_;
 		}
 		else
@@ -175,7 +251,10 @@ namespace manipulator
 			pow(torqueInfo_.Actuators.Actuator6, 2) > pow(maxTorqJoint6_, 2))
 		{
 			kComm_.myStopControlAPI();
-			ROS_ERROR("Torque limit exceeded. Stopping the arm.");
+			if (!torqueFault_)
+			{
+				ROS_ERROR("Torque limit exceeded. Stopping the arm.");
+			}
 			torqueFault_ = true;
 		}
 	}
@@ -189,20 +268,25 @@ namespace manipulator
 			sensor_msgs::JointState js;
 			js.header.stamp = ros::Time::now();
 			js.header.frame_id = frameId_;
-			js.name = activeJoints_;
+			js.name = baseGroupActiveJoints_;
 
-			std::vector<double> tmp;
+			std::vector<float> tmp;
 
-			tmp.push_back((double)angularInfo_.Actuators.Actuator1);
-			tmp.push_back((double)angularInfo_.Actuators.Actuator2);
-			tmp.push_back((double)angularInfo_.Actuators.Actuator3);
-			tmp.push_back((double)angularInfo_.Actuators.Actuator4);
-			tmp.push_back((double)angularInfo_.Actuators.Actuator5);
-			tmp.push_back((double)angularInfo_.Actuators.Actuator6);
+			tmp.push_back(angularInfo_.Actuators.Actuator1 - jointsOffset_.at(0));
+			tmp.push_back(angularInfo_.Actuators.Actuator2 - jointsOffset_.at(1));
+			tmp.push_back(angularInfo_.Actuators.Actuator3 - jointsOffset_.at(2));
+			tmp.push_back(angularInfo_.Actuators.Actuator4 - jointsOffset_.at(3));
+			tmp.push_back(angularInfo_.Actuators.Actuator5 - jointsOffset_.at(4));
+			tmp.push_back(angularInfo_.Actuators.Actuator6 - jointsOffset_.at(5));
 
-			for (unsigned int j = 0; j < nbJoints_; j++)
+			for (unsigned int i = 0; i < baseGroupNbJoints_ - manipulatorGroupNbJoints_; i++)
 			{
-				js.position.push_back(tmp[j]);
+				js.position.push_back(0.0);
+			}
+
+			for (unsigned int j = 0; j < manipulatorGroupNbJoints_; j++)
+			{
+				js.position.push_back(degToRad(tmp.at(j)));
 			}
 
 			jointStatePub_.publish(js);
@@ -213,13 +297,13 @@ namespace manipulator
 		}
 		return;
 	}
-
+/*
 	void wmArm::velocityCallback(const ros::TimerEvent& e)
 	{
 		// TODO
 		return;
 	}
-
+*/
 	int wmArm::recoverTorqueFault()
 	{
 		if (torqueFault_)
@@ -241,14 +325,43 @@ namespace manipulator
 		}
 	}
 
-	bool wmArm::computePlan(const geometry_msgs::Pose& target, moveit::planning_interface::MoveGroup::Plan& myPlan)
+	bool wmArm::computeBasePlan(const geometry_msgs::PoseStamped& target, moveit::planning_interface::MoveGroup::Plan& myPlan)
 	{
-		moveGroup_.setPoseTarget(target);
-		bool planSuccess = moveGroup_.plan(myPlan);
-		moveGroup_.clearPoseTargets();
+		if (!baseMoveGroup_.setPoseTarget(target))
+		{
+			return false;
+		}
 
+		baseMoveGroup_.setStartStateToCurrentState();
+		bool planSuccess = baseMoveGroup_.plan(myPlan);
+		baseMoveGroup_.clearPoseTargets();
 		return planSuccess;
 	}
 
+	bool wmArm::computeManipulatorPlan(const geometry_msgs::PoseStamped& target, moveit::planning_interface::MoveGroup::Plan& myPlan)
+	{
+		if (!manipulatorMoveGroup_.setPoseTarget(target))
+		{
+			return false;
+		}
+
+		manipulatorMoveGroup_.setStartStateToCurrentState();
+		bool planSuccess = manipulatorMoveGroup_.plan(myPlan);
+		manipulatorMoveGroup_.clearPoseTargets();
+		return planSuccess;
+	}
+
+	bool wmArm::computeManipulatorPlan(const std::vector<double>& target, moveit::planning_interface::MoveGroup::Plan& myPlan)
+	{
+		if (!manipulatorMoveGroup_.setJointValueTarget(target))
+		{
+			return false;
+		}
+
+		manipulatorMoveGroup_.setStartStateToCurrentState();
+		bool planSuccess = manipulatorMoveGroup_.plan(myPlan);
+		manipulatorMoveGroup_.clearPoseTargets();
+		return planSuccess;
+	}
 } // namespace manipulator
 
