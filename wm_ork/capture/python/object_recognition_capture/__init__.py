c871820339117ff9544540623fcd90e2e91c2dfc
Samuel Otis <sotistechno@gmail.com>
2016-06-04 23:56:01 -0400

    retrait wm_ork : defectueux

--- wm_ork/capture/python/object_recognition_capture/__init__.py
+++
@@ -1,3 +0,0 @@
-from .fiducial_pose_est import *
-from .orb_capture import *
-from .ecto_cells.capture import *
--- wm_ork/object_recognition_core/python/object_recognition_core/db/cells.py
+++
@@ -1,2 +0,0 @@
-# Use a specific name in the imports intead of an import * to make sure of what we have
-from object_recognition_core.ecto_cells.db import ModelWriter, ObservationInserter, ObservationReader
