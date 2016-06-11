9945aa3a28c7460431e71481a5fcb3c75b4a260d
Thierry Pouplier <thierrypouplier@gmail.com>
2016-06-11 14:50:35 -0400

    update neck joint state

--- wm_dynamixel_controller/src/neckHead.py
+++ wm_dynamixel_controller/src/neckHead.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 import roslib
-
 import rospy
 from std_msgs.msg import Float64
 import dynamixel_msgs.msg
 import sensor_msgs.msg
+from threading import Lock
+
+mutex = Lock()
 
 class dynamixelState:
     def __init__(self):
         self.pub = rospy.Publisher('/neckHead/state', sensor_msgs.msg.JointState, queue_size=1)
         self.sub = rospy.Subscriber('/neckHead_controller/state', dynamixel_msgs.msg.JointState, self.callback)
         self.msg = sensor_msgs.msg.JointState()
-	self.msg.name.append(0)
+        self.msg.name.append(0)
         self.msg.velocity.append(0)
         self.msg.position.append(0)
         self.msg.effort.append(0)
 
     def callback(self, data):
+        mutex.acquire()
         self.msg.name[0] = data.name
         self.msg.position[0] = data.current_pos
         self.msg.velocity[0] = data.velocity
         self.msg.effort[0] = 0
+        mutex.release()
 
     def publish(self):
+        mutex.acquire()
         self.pub.publish(self.msg)
+        mutex.release()
 
 def main():
     rospy.init_node('dynamixel_publisher')
