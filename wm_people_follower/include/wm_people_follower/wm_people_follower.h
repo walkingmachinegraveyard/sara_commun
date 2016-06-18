e122156280353a77e5f09e98b932fd4c3eb77ed5
xhache <xvr.hache@gmail.com>
2016-06-18 13:25:53 -0400

    new package

---
+++ wm_people_follower/include/wm_people_follower/wm_people_follower.h
@@ -0,0 +1,49 @@
+/*
+ * wm_people_follower.h
+ *
+ *  Created on: Jun 5, 2016
+ *      Author: xhache
+ */
+
+#ifndef WM_PEOPLE_FOLLOWER_H_
+#define WM_PEOPLE_FOLLOWER_H_
+
+#include <math.h>
+
+#include "ros/ros.h"
+#include "actionlib/client/simple_action_client.h"
+#include "people_msgs/PositionMeasurementArray.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "wm_people_follower/peopleFollower.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <boost/thread/lock_guard.hpp>
+
+namespace wm
+{
+	class peopleFollower
+	{
+		public:
+			peopleFollower(const ros::NodeHandle&);
+			~peopleFollower();
+			bool peopleFollowerService(wm_people_follower::peopleFollower::Request&, wm_people_follower::peopleFollower::Response&);
+
+		private:
+			void legTrackerCallback(const people_msgs::PositionMeasurementArray&);
+			ros::NodeHandle nh_;
+			geometry_msgs::Point personNewPosition_, personPreviousPosition_;
+			actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> moveBaseAC_;
+			ros::Subscriber legTrackerSub_;
+			ros::ServiceServer followSrvServer_;
+			ros::Publisher testPub_;
+			boost::mutex mtx_;
+			ros::Timer lockTargetTimer_;
+			bool isFollowing_, lockTarget_, targetLocked_;
+			double followDistance_, maxLockDistance_, searchDistance_;
+			double minMeasurementReliability_;
+			double lastActionTime_, actionClientPeriod_;
+			int lockingTimeOut_;
+	};
+} // namespace wm
+#endif /* WM_PEOPLE_FOLLOWER_H_ */
