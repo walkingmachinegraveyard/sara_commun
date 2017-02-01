6cb5700b55f3114ba72388ec6a5fba56ad2de573
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2017-02-01 02:14:41 -0500

    proof of concept for tts node

---
+++ wm_tts/src/wm_tts_node_service.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import pyttsx
+import rospy
+import time
+import threading
+from std_msgs.msg import String
+from wm_tts.srv import *
+
+
+class wm_tts():
+    def __init__(self, node_name):
+        # get parameters
+        rospy.init_node(node_name)
+        s = rospy.Service('wm_say', say_service, self.say)
+        self.engine = pyttsx.init()
+        self.engine_thread = threading.Thread(target=self.loop)
+        self.engine_thread.start()
+        rate = rospy.get_param('rate', 150)
+        self.engine.setProperty('rate', rate)
+        self.engine.setProperty('volume', 0.5)
+
+    def loop(self):
+        self.engine.startLoop(False)
+        while not rospy.is_shutdown():
+            self.engine.iterate()
+            time.sleep(0.1)
+        self.engine.endLoop()
+
+    def say(self, req):
+        rospy.loginfo(req.say)
+        self.engine.say(req.say)
+        while self.engine.isBusy():
+            time.sleep(0.1)
+
+        return True
+
+
+if __name__ == '__main__':
+
+    try:
+
+        wm_tts('wm_tts_node')
+
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass
